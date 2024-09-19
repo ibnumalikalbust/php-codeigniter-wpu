96b61a821b2c182b4306833361964c3cbe2eec2a
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-19 20:58:05 +0700

    validation

--- app/Controllers/Comic.php
+++ app/Controllers/Comic.php
@@ -23,25 +23,39 @@ class Comic extends BaseController
     public function detail($slug = null)
     {
 		$data['title'] = 'Comic Detail';
-        if (is_null($slug)) {
-            return redirect()->to(base_url('/comic'));
-        } else {
-            $data['comic'] = $this->model->getFirstComicBySlug($slug);
-            return view('comic/detail', $data);
-        }
+        $data['comic'] = $this->model->getFirstComicBySlug($slug);
+        return view('comic/detail', $data);
     }
 
     public function insertget()
     {
         $data['title'] = 'Comic Insert';
+        $flashData = session()->getFlashdata();
+        if (isset($flashData['_ci_old_input']['post'])) {
+            $data['postInput'] = $flashData['_ci_old_input']['post'];
+        }
+        if (isset($flashData['_ci_validation_errors'])) {
+            $data['postError'] = $flashData['_ci_validation_errors'];
+        }
         return view('comic/insert', $data);
     }
 
     public function insertpost()
     {
-        $data = $this->request->getPost();
-        $message = $this->model->insertNewComic($data);
-        session()->setFlashData('message', $message);
-        return redirect()->to(base_url(('/comic')));
+        $validation = \Config\Services::validation();
+        $validation->setRules([
+            'title' => 'required|is_unique[comic.title]',
+            'author' => 'required',
+            'publisher' => 'required',
+            'image' => 'required'
+        ]);
+        if ($validation->withRequest($this->request)->run() == true) {
+            $data = $this->request->getPost();
+            $message = $this->model->insertNewComic($data);
+            session()->setFlashData('message', $message);
+            return redirect()->to(base_url(('/comic')));
+        } else {
+            return redirect()->to(base_url('/comic/insertget'))->withInput();
+        }
     }
 }
--- app/Views/comic/insert.php
+++ app/Views/comic/insert.php
@@ -14,30 +14,47 @@
 	<div class="container max-w-500-px">
 		<div class="row">
 			<div class="col">
+				<p></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
 				<form action="<?= base_url('/comic/insertpost'); ?>" method="post" autocomplete="off">
 					<?= csrf_field(); ?>
 					<div class="row mb-3">
 						<label for="title" class="col-sm-2 col-form-label">Title</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" id="title" name="title" required autofocus>
+							<input type="text" class="form-control <?= isset($postError['title']) ? 'is-invalid' : ''; ?>" id="title" name="title" value="<?= isset($postInput['title']) ? $postInput['title'] : ''; ?>">
+							<div class="invalid-feedback">
+								<span><?= isset($postError['title']) ? $postError['title'] : ''; ?></span>
+							</div>
 						</div>
 					</div>
 					<div class="row mb-3">
 						<label for="author" class="col-sm-2 col-form-label">Author</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" id="author" name="author" required>
+							<input type="text" class="form-control <?= isset($postError['author']) ? 'is-invalid' : ''; ?>" id="author" name="author" value="<?= isset($postInput['author']) ? $postInput['author'] : ''; ?>">
+							<div class="invalid-feedback">
+								<span><?= isset($postError['author']) ? $postError['author'] : ''; ?></span>
+							</div>
 						</div>
 					</div>
 					<div class="row mb-3">
 						<label for="publisher" class="col-sm-2 col-form-label">Publisher</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" id="publisher" name="publisher" required>
+							<input type="text" class="form-control <?= isset($postError['publisher']) ? 'is-invalid' : ''; ?>" id="publisher" name="publisher" value="<?= isset($postInput['publisher']) ? $postInput['publisher'] : ''; ?>">
+							<div class="invalid-feedback">
+								<span><?= isset($postError['publisher']) ? $postError['publisher'] : ''; ?></span>
+							</div>
 						</div>
 					</div>
 					<div class="row mb-3">
 						<label for="image" class="col-sm-2 col-form-label">Image</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" id="image" name="image" required>
+							<input type="text" class="form-control <?= isset($postError['image']) ? 'is-invalid' : ''; ?>" id="image" name="image" value="<?= isset($postInput['image']) ? $postInput['image'] : ''; ?>">
+							<div class="invalid-feedback">
+								<span><?= isset($postError['image']) ? $postError['image'] : ''; ?></span>
+							</div>
 						</div>
 					</div>
 					<button type="submit" class="btn btn-primary">INSERT</button>
