bca1c35dbb0797ed7d26fc691d0961aee274912d
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-21 11:20:57 +0700

    bug fixed

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 function changePreviewImage() {
-	const imageInput = document.getElementById('image-file');
+	const imageInput = document.getElementById('image');
 	const imagePreview = document.getElementById('img-preview');
 	const imageFile = new FileReader();
 	imageFile.readAsDataURL(imageInput.files[0]);
