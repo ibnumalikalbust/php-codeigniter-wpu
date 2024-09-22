e52d29aaafea7414619e28902146130c694ef6b5
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-22 12:51:15 +0700

    update migrate

--- app/Database/Migrations/2024-09-22-030823_TestingStudent.php
+++ app/Database/Migrations/2024-09-22-030823_TestingStudent.php
@@ -10,11 +10,48 @@ class TestingStudent extends Migration
     
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'nrp' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'major' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable($this->table);
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable($this->table);
     }
 }
