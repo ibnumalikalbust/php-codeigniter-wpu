955f0ec58d8df2468beffc0f5974f9a861e0d043
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-27 18:37:17 +0700

    change fazzdev css

--- app/Views/profile/template.php
+++ app/Views/profile/template.php
@@ -6,7 +6,7 @@
 	<title><?= $title; ?></title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.css">
-	<link rel="stylesheet" href="https://fazzdev.com/vendor/local/2024.css">
+	<link rel="stylesheet" href="https://fazzdev.com/css/2024.css">
 	<meta charset="UTF-8">
 </head>
 
